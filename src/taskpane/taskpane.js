7e822b0c2531020484a7e5ffa6dde8fac9d29135
Matthew Calvert <mattc@themailshark.com>
2022-07-20 09:15:19 -0400

    potential bug fix and small easter egg update

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,16 +10,17 @@ $(() => {
     $("#meece").on("click", () => {
         //location.reload();
         console.log("CLICKED🐭");
-        showFissh("show");
+        showElement("#fissh", "show");
     })
 
     $(".ok").on("click", function() {
-        showFissh("hide");
-        location.reload();
+        showElement("#fissh", "hide");
+        showFisshGif();
+        //location.reload();
     });
 
     $(".dont").on("click", function() {
-        showFissh("hide");
+        showElement("#fissh", "hide");
     });
 
 
@@ -4086,13 +4087,32 @@ $(() => {
     }
 
 
-    function showFissh(showHide) {
+    // function showFissh(showHide) {
+    //     if (showHide === "hide") {
+    //         $("#fissh").css("display", "none");
+    //     } else if (showHide === "show") {
+    //         $("#fissh").css("display", "flex");
+    //     };
+    // };
+
+    function showElement(element, showHide) {
         if (showHide === "hide") {
-            $("#fissh").css("display", "none");
+            $(element).css("display", "none");
         } else if (showHide === "show") {
-            $("#fissh").css("display", "flex");
+            $(element).css("display", "flex");
         };
     };
+
+    function showFisshGif() {
+        $("#fissh-gif").css("display", "flex");
+        setTimeout(hideFisshGif, 2000);
+    };
+
+    function hideFisshGif() {
+        $("#fissh-gif").css("display", "none");
+    };
+
+    
 
     //#region CHECK EVENTS -----------------------------------------------------------------------------------------------------------------------
 
@@ -4582,8 +4602,8 @@ $(() => {
 
         //#region TRY CATCH ---------------------------------------------------------------------------------------------
             async function tryCatch(callback) {
-                console.log("Error callback type is: ");
-                console.log(typeof callback);
+                // console.log("Error callback type is: ");
+                // console.log(typeof callback);
                 //if (typeof callback === 'function') {
                     try {
                         await callback();
